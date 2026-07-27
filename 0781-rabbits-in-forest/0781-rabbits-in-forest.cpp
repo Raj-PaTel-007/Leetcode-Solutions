793415e46828b4d86706cef9f690a9ef82dc5766
Raj-PaTel-007 <rajpatel92879@gmail.com>
2026-07-28 01:20:05 +0530

    Time: 3 ms (22.57%), Space: 12.3 MB (37.7%) - LeetHub

---
+++ 0781-rabbits-in-forest/0781-rabbits-in-forest.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int numRabbits(vector<int>& arr) {
+        unordered_map<int,int>mp;
+        for(auto it : arr)mp[it]++;
+        int ans = 0;
+        for(auto it : mp){
+            int a = it.first;
+            int b = it.second;
+            if(a == 0){
+                ans += b;
+                continue;
+            }
+            int cnt = b/(a+1);
+            if(b % (a+1) == 0) ans += cnt*(a+1);
+            else ans += (cnt +1)*(a+1); 
+        }
+        return ans;
+    }
+};
